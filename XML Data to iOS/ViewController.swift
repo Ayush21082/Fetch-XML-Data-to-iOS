3d96fc36a37a7ce05ac64a344a76c4e198ad54ae
Ayush Singh <ayush.singh21082@gmail.com>
2022-03-26 13:57:45 +0530

    Initial Commit

---
+++ XML Data to iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XML Data to iOS
+//
+//  Created by Ayush Singh on 26/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
