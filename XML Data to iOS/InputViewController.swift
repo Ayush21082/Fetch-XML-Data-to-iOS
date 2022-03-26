e94b9cc1ed9a1f43321db0b2960ac370470b78c5
Ayush Singh <ayush.singh21082@gmail.com>
2022-03-26 15:58:58 +0530

    🥳

--- XML Data to iOS/InputViewController.swift
+++ XML Data to iOS/InputViewController.swift
@@ -9,21 +9,53 @@ import UIKit
 
 class InputViewController: UIViewController {
 
+    
+    let textView = UITextView()
+    let myFirstButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        display()
+            
 
-        // Do any additional setup after loading the view.
+        }
+
+        @objc func pressed() {
+            if textView.text != "" {
+                url = textView.text
+                
+                self.performSegue(withIdentifier: "next", sender: self)
+
+                
+                
+            }
+        }
+
+    
+    func display() {
+        //Text view
+        textView.frame = CGRect(x: 20.0, y: 90.0, width: 250.0, height: 100.0)
+        textView.contentInsetAdjustmentBehavior = .automatic
+        textView.center = self.view.center
+        textView.textAlignment = NSTextAlignment.justified
+        textView.textColor = UIColor.white
+        textView.backgroundColor = UIColor.lightGray
+        view.addSubview(textView)
+        
+        //Button
+        
+        myFirstButton.setTitle("Submit", for: .normal)
+        myFirstButton.setTitleColor(.black, for: .normal)
+        myFirstButton.backgroundColor = .cyan
+        myFirstButton.center = self.view.center
+        
+        myFirstButton.frame = CGRect(x: 15, y: 50, width: view.frame.width, height: 50)
+        myFirstButton.addTarget(self, action: #selector(pressed), for: .touchUpInside)
+        myFirstButton.frame.origin = CGPoint(x:0, y:self.view.frame.size.height - myFirstButton.frame.size.height - 20)
+        
+        view.addSubview(myFirstButton)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- XML Data to iOS/ViewController.swift
+++ XML Data to iOS/ViewController.swift
@@ -7,13 +7,195 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var url = String()
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    private var myTableView: UITableView!
+    
+    
+    
+    let recordKey = "ListOfItems"
+    let dictionaryKeys = Set<String>(["StationId","StationName","Logo"])
+    
+    
+    
+    var results: [[String: String]]?
+    
+    var currentDictionary: [String: String]?
+    var currentValue: String?
+    
+    var StationName = [String]()
+    var StationId = [String]()
+    var Logo = [String]()
+    
+    var activityView = UIActivityIndicatorView(style: .large)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        myTableView = UITableView(frame: CGRect(x: 0, y: 0, width: displayWidth, height: displayHeight))
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        
+        self.view.addSubview(myTableView)
+        
+        
+        
+        
+        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        loadData()
+    }
+    
+    func showActivityIndicatory() {
+        
+       activityView.center = self.view.center
+       self.view.addSubview(activityView)
+       activityView.startAnimating()
+   }
 
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Num: \(indexPath.row)")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return StationId.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        cell.textLabel!.text = "\(StationName[indexPath.row])"
+        
+        
+        let url = URL(string: Logo[indexPath.row])
+        let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+        cell.imageView?.image = UIImage(data: data!)
+        
+        return cell
+    }
+    
+    
 }
+
+extension ViewController: XMLParserDelegate {
+    
+    func loadData() {
+        
+        showActivityIndicatory()
+        
+        let url = URL(string: url)!
+        let request=URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("dataTaskWithRequest error: \(error)")
+                return
+            }
+            guard let data = data else {
+                print("dataTaskWithRequest data is nil")
+                return
+            }
+            
+            
+            
+            let parser = XMLParser(data: data)
+            parser.delegate = self
+            if parser.parse() {
+                
+                
+                
+                var set = Set<String>()
+                let arraySet: [[String : String]] = self.results!.compactMap {
+                    guard let name = $0["Logo"] else {return nil }
+                    return set.insert(name).inserted ? $0 : nil
+                }
+                
+                self.Logo = arraySet.compactMap { $0["Logo"] }
+                self.StationName = arraySet.compactMap { $0["StationName"] }
+                self.StationId = arraySet.compactMap { $0["StationId"] }
+                
+                self.results = arraySet
+    
+    
+                print(self.results!)
+                
+            }
+            
+            DispatchQueue.main.async {
+                self.activityView.stopAnimating()
+                self.activityView.removeFromSuperview()
+                self.myTableView.reloadData()
+                
+            }
+            
+        }
+        task.resume()
+    }
+    
+    
+    func parserDidStartDocument(_ parser: XMLParser) {
+        results = []
+    }
+    
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        if elementName == recordKey {
+            currentDictionary = [:]
+        } else if dictionaryKeys.contains(elementName) {
+            currentValue = ""
+        }
+    }
+    
+    
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        currentValue? += string
+    }
+    
+    
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == recordKey {
+            results!.append(currentDictionary!)
+            currentDictionary = nil
+        } else if dictionaryKeys.contains(elementName) {
+            currentDictionary![elementName] = currentValue
+            results!.append(currentDictionary!)
+            
+            currentValue = nil
+        }
+    }
+    
+    
+    
+    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+        print(parseError)
+        
+        currentValue = nil
+        currentDictionary = nil
+        results = nil
+    }
+    
+    
+}
+
+struct Item {
+    var StationId: String
+    var StationName: String
+    var Logo: String
+}
+
 
